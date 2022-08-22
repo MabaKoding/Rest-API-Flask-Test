b055fabcb3ebd14f25b61e919b182ae7b3282468
MabaKoding <nuryasin.daudpoetra@gmail.com>
2022-08-22 22:19:25 +0700

    inisiasi resource

---
+++ app.py
@@ -0,0 +1,41 @@
+# import libary
+# from crypt import methods
+# from urllib import response
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+# inisiasi object flask
+app = Flask(__name__)
+
+# inisiasi object flask_restful
+api = Api(app)
+
+# insiasi object flask_cors
+CORS(app)
+
+# inisiasi variabel kosong bertipe dictionaru
+identitas = {} # variabel global 
+
+# membuat class resources
+class ContohResource(Resource) :
+    # metode get dan post
+    def get(self) :
+        # response = {"msg" : "Hallo dunia, ini app restful pertamaku"}
+        return identitas
+    
+    def post(self) :
+        nama = request.form["nama"]
+        umur = request.form["umur"]
+        identitas["nama"] = nama 
+        identitas["umur"] = umur
+        response = {"msg" : "Data berhasil dimasukkan"}
+        return response
+
+# setup resourcenya
+api.add_resource(ContohResource, "/api", methods=["GET","POST"])
+
+if __name__ == "__main__" :
+    app.run(debug=True, port=5005)
+
+  
